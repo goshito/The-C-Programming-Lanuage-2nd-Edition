56345fc9888efcb8739caa0a27861a5a73ede020
Georgi <Georgi@Georgi-PC>
2016-02-17 13:17:18 +0200

    section 1.3 The for statement, page 13

--- main.c
+++ main.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-//celsius to fahr table over 20, using celsius = 5*(fahr-32)/9, p.25 2nd ver with float
+//section 1.3 The for statement, page 13
 int main() {
-    float fahr, celsius;
-    int lower, upper, step;
+    int fahr;
     
-    lower = 0;
-    upper = 300;
-    step = 20;
-    
-    printf("Celsius:\tFahreinheit:\n");
-    celsius = lower;
-    while (celsius <= 300) {
-        fahr = celsius * 9.0/5.0 + 32.0;
-        printf("%.1f\t\t%.1f\n", celsius, fahr);
-        celsius = celsius + step;
+    for (fahr = 0; fahr <= 300; fahr += 20) {
+        printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     }
     return 0;
 }
