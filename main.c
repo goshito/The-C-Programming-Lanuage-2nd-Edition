a338719e4c5d6197eb01fe41511615f3e5da0929
Georgi <Georgi@Georgi-PC>
2016-02-19 11:38:53 +0200

    //17

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
-//16, File copying
+//17, 
 #include <stdio.h>
 
 int main() {
     int c;
     
-    c = getchar();
-    while (c != EOF) {
+    while ((c = getchar()) != EOF) {
         putchar(c);
-        c = getchar();
     }
     return 0;
 }
