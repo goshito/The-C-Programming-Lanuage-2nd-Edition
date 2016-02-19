9cb2f30c1964fed34c3eeb0a1461876e4164d237
Georgi <Georgi@Georgi-PC>
2016-02-19 11:20:46 +0200

    //15, Symbolic constants

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
+//15, Symbolic constants
 #include <stdio.h>
-//section 1.3 The for statement, page 13
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
     int fahr;
     
-    for (fahr = 0; fahr <= 300; fahr += 20) {
+    for (fahr = LOWER; fahr <= UPPER; fahr += STEP) {
         printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     }
     return 0;
