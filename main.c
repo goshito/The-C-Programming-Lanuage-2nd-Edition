0232bec0dfad46416d641345151eec621e34f8ff
Georgi <Georgi@Georgi-PC>
2016-02-23 10:53:50 +0200

    24, 1.7 Functions

--- main.c
+++ main.c
@@ -1,26 +1,23 @@
-//22, 1.6 Arrays,  count digits, white space, others 
+//24, 1.7 Functions 
 #include <stdio.h>
-// count digits, white space, others
+
+int power(int m, int n);
+
+//test power function
 int main() {
-    int c, i, nwhite, nother;
-    int ndigit[10];
+    int i;
     
-    nwhite = nother = 0;
-    for (i = 0; i < 10; ++i) 
-        ndigit[i] = 0;
-    
-    while ( (c = getchar())!= EOF)
-        if (c >= '0' && c <= '9')
-            ++ndigit[c-'0'];
-        else if (c == ' ' || c == '\n' || c == '\t')
-            ++nwhite;
-        else
-            ++nother;
-        
-    printf("digits = ");
     for (i = 0; i < 10; ++i)
-        printf("%d", ndigit[i]);
-    printf(", whitespace = %d, other = %d\n", nwhite, nother);
-        
+        printf("%d %d %d\n", i, power(2, i), power(-3, i));
     return 0;
+}
+
+// power: raise base to the n-th power; n >= 0
+int power(int base, int n) {
+    int i, p;
+    
+    p = 1;
+    for (i = 1; i <= n; ++i)
+        p = p * base;
+    return p;
 }
